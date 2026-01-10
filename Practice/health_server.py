814b58d5e4967c13629632e84125ebdc6b03a35a
Vibhuti Jain <vibhutijain9812@gmail.com>
2026-01-10 23:49:50 +0530

    Create health_server.py

---
+++ Practice/health_server.py
@@ -0,0 +1,26 @@
+Ques: Remote Server Health Check - Write a script that: Pings a list of servers Reports which servers are UP or DOWN
+import socket
+
+servers = [
+    ("google.com", 80),
+    ("localhost", 22),
+    ("example.com", 443)
+]
+
+def is_host_up(): 
+    timeout = 3
+
+    for host, port in servers:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(timeout)
+
+        try:
+         sock.connect((host, port))
+         print(f"{host}:{port} is UP")
+        except:
+         print(f"{host}:{port} is DOWN")
+        finally:
+         sock.close()
+
+
+is_host_up()
